a6a3a107007ea6abdb0ff86aa80420c7a3a0a657
Tim Hutt <tdhutt@gmail.com>
2023-05-12 08:54:46 +0100

    Switch to mutex & thread local data
    Also use the time crate to format the times. It's a bit simpler.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::{Path, PathBuf};
-use std::sync::atomic::{AtomicBool, AtomicU64};
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::AtomicBool;
 
 use anyhow::{anyhow, bail, Context, Result};
 use clap::Parser;
@@ -16,19 +17,82 @@ struct Cli {
     stamp: Option<PathBuf>,
 }
 
-use std::time::UNIX_EPOCH;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-pub fn mtime(path: &Path) -> Result<u64> {
-    // 64 bits of nanoseconds gives 500 years so we should be fine still.
-    let nanos = std::fs::metadata(path)
-        .with_context(|| anyhow!("error reading file metadata for {}", path.display()))?
-        .modified()
-        .with_context(|| anyhow!("error reading mtime for {}", path.display()))?
-        .duration_since(UNIX_EPOCH)
-        .with_context(|| anyhow!("file modified before UNIX Epoch {}", path.display()))?
-        .as_nanos();
+pub fn mtime(path: &Path) -> std::io::Result<SystemTime> {
+    std::fs::metadata(path)?.modified()
+}
 
-    u64::try_from(nanos).with_context(|| anyhow!("mtime too far in future {nanos}"))
+struct MtimeVisitor {
+    // Max mtime for the thread.
+    thread_max_mtime: SystemTime,
+    // Max mtime for all threads.
+    max_mtime: Arc<Mutex<SystemTime>>,
+    // If any thread had an error.
+    error: Arc<AtomicBool>,
+}
+
+impl MtimeVisitor {
+    fn new(max_mtime: Arc<Mutex<SystemTime>>, error: Arc<AtomicBool>) -> Self {
+        Self {
+            thread_max_mtime: UNIX_EPOCH,
+            max_mtime,
+            error,
+        }
+    }
+}
+
+impl Drop for MtimeVisitor {
+    fn drop(&mut self) {
+        let mut max_mtime = self.max_mtime.lock().unwrap();
+        *max_mtime = max_mtime.max(self.thread_max_mtime);
+    }
+}
+
+impl ignore::ParallelVisitor for MtimeVisitor {
+    fn visit(&mut self, entry: std::result::Result<ignore::DirEntry, ignore::Error>) -> ignore::WalkState {
+        match entry {
+            Ok(entry) => match mtime(entry.path()) {
+                Ok(mtime) => {
+                    self.thread_max_mtime = self.thread_max_mtime.max(mtime);
+                    ignore::WalkState::Continue
+                }
+                Err(e) => {
+                    eprintln!(
+                        "Error getting mtime for path {}: {e}",
+                        entry.path().display()
+                    );
+                    self.error.store(true, std::sync::atomic::Ordering::SeqCst);
+                    ignore::WalkState::Quit
+                }
+            },
+            Err(e) => {
+                eprintln!("Error: {e}");
+                self.error.store(true, std::sync::atomic::Ordering::SeqCst);
+                ignore::WalkState::Quit
+            }
+        }
+    }
+}
+
+struct MtimeVisitorBuilder {
+    // Max mtime overall.
+    max_mtime: Arc<Mutex<SystemTime>>,
+    // If any thread had an error.
+    error: Arc<AtomicBool>,
+}
+
+impl Default for MtimeVisitorBuilder {
+    fn default() -> Self {
+        Self { max_mtime: Arc::new(Mutex::new(UNIX_EPOCH)), error: Arc::new(AtomicBool::new(false)) }
+    }
+}
+
+impl<'s> ignore::ParallelVisitorBuilder<'s> for MtimeVisitorBuilder
+{
+    fn build(&mut self) -> Box<dyn ignore::ParallelVisitor + 's> {
+        Box::new(MtimeVisitor::new(self.max_mtime.clone(), self.error.clone()))
+    }
 }
 
 fn main() -> Result<()> {
@@ -36,60 +100,33 @@ fn main() -> Result<()> {
 
     let path = cli.path.as_deref().unwrap_or_else(|| Path::new("."));
 
-    let mut max_mtime = AtomicU64::new(0);
-    let mut error = AtomicBool::new(false);
-
-    let visitor =
-        |path: std::result::Result<ignore::DirEntry, ignore::Error>| -> ignore::WalkState {
-            match path {
-                Ok(path) => match mtime(path.path()) {
-                    Ok(mtime) => {
-                        max_mtime.fetch_max(mtime, std::sync::atomic::Ordering::SeqCst);
-                        ignore::WalkState::Continue
-                    }
-                    Err(e) => {
-                        eprintln!(
-                            "Error getting mtime for path {}: {e}",
-                            path.path().display()
-                        );
-                        error.store(true, std::sync::atomic::Ordering::SeqCst);
-                        ignore::WalkState::Quit
-                    }
-                },
-                Err(e) => {
-                    eprintln!("Error: {e}");
-                    error.store(true, std::sync::atomic::Ordering::SeqCst);
-                    ignore::WalkState::Quit
-                }
-            }
-        };
+    let mut visitor_builder = MtimeVisitorBuilder::default();
 
     WalkBuilder::new(path)
         .build_parallel()
-        .run(|| Box::new(visitor));
+        .visit(&mut visitor_builder);
 
-    let error = *error.get_mut();
-    let max_mtime = *max_mtime.get_mut();
+    let error = visitor_builder.error.load(std::sync::atomic::Ordering::SeqCst);
+    let max_mtime = visitor_builder.max_mtime.lock().unwrap().clone();
 
     if error {
         // Error already printed above.
         bail!("");
     }
 
+    let max_mtime_nanos = time::OffsetDateTime::from(max_mtime).unix_timestamp_nanos();
+
     // Print the maximum mtime.
-    println!("{}", max_mtime);
+    println!("{}", max_mtime_nanos);
 
     // If requested save it to a file and set that file's mtime to the
     // maximum mtime.
     if let Some(stamp) = &cli.stamp {
-        std::fs::write(stamp, format!("{}\n", max_mtime))
+        std::fs::write(stamp, format!("{}\n", max_mtime_nanos))
             .with_context(|| anyhow!("error writing stamp file {}", stamp.display()))?;
         filetime::set_file_mtime(
             stamp,
-            filetime::FileTime::from_unix_time(
-                (max_mtime / 1_000_000_000) as i64,
-                (max_mtime % 1_000_000_000) as u32,
-            ),
+            filetime::FileTime::from_system_time(max_mtime),
         )
         .with_context(|| anyhow!("error setting mtime of stamp file {}", stamp.display()))?;
     }
